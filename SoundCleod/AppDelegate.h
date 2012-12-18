99a5dfe15e3bbf32f72dfecc2cfa7e7e6eb1f618
Márton Salomváry <salomvary@gmail.com>
2012-12-18 10:59:15 +0100

    rebranding:)

--- SoundCleod/AppDelegate.h
+++ SoundCleod/AppDelegate.h
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.h
-//  SoundCloud NEXT
+//  SoundCleod
 //
 //  Created by Márton Salomváry on 2012/12/11.
 //  Copyright (c) 2012 Márton Salomváry. All rights reserved.
--- SoundCleod/main.m
+++ SoundCleod/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  SoundCloud NEXT
+//  SoundCleod
 //
 //  Created by Márton Salomváry on 2012/12/11.
 //  Copyright (c) 2012 Márton Salomváry. All rights reserved.
