f603f8d392b6c9f8990757878e60d25d6b823106
Márton Salomváry <salomvary@gmail.com>
2016-09-16 16:32:17 +0200

    Remove all the Objective-C nonsense :)

--- SoundCleod/AppConstants.c
+++
@@ -1,9 +0,0 @@
-//
-//  AppConstants.c
-//  SoundCleod
-//
-//  Created by Márton Salomváry on 2013/01/17.
-//  Copyright (c) 2013 Márton Salomváry. All rights reserved.
-//
-
-NSString *const SCHost = @"soundcloud.com";
