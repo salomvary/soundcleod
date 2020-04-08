f8c328726fe18484c450fa9030c74fcadbcf727e
Márton Salomváry <salomvary@gmail.com>
2020-04-08 10:32:53 +0200

    Fix login tests

--- test/login-test.js
+++ test/login-test.js
@@ -8,11 +8,19 @@ describe('Logging in', function() {
 
   it('shows Facebook login in a popup', function() {
     return this.app.client
+      // Click sign in button
       .waitForVisible('button=Sign in')
       .element('#content')
       .click('button=Sign in')
+      // Wait for the modal to show up
+      .waitForVisible('.modal')
+      // Switch to the iframe inside the modal that contains the actual login buttons
+      .element('.modal iframe')
+      .then((iframe) => this.app.client.frame(iframe.value))
+      // Click the Facebook button once rendered
       .waitForVisible('button=Continue with Facebook')
       .click('button=Continue with Facebook')
+      // Wait for new window(s) to open
       .waitUntil(() =>
         this.app.client.getWindowCount().then((count) => count > 1)
       )
@@ -22,11 +30,19 @@ describe('Logging in', function() {
 
   it('shows Google login in a popup', function() {
     return this.app.client
+      // Click sign in button
       .waitForVisible('button=Sign in')
       .element('#content')
       .click('button=Sign in')
+      // Wait for the modal to show up
+      .waitForVisible('.modal')
+      // Switch to the iframe inside the modal that contains the actual login buttons
+      .element('.modal iframe')
+      .then((iframe) => this.app.client.frame(iframe.value))
+      // Click the Google button once rendered
       .waitForVisible('button=Continue with Google')
       .click('button=Continue with Google')
+      // Wait for new window(s) to open
       .waitUntil(() =>
         this.app.client.getWindowCount().then((count) => count > 1)
       )
