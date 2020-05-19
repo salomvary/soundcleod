2b744bf005277eb87710f2ceebfcbe28db8e9dbf
Márton Salomváry <salomvary@gmail.com>
2020-05-19 13:36:51 +0200

    Fix style with Prettier

--- test/login-test.js
+++ test/login-test.js
@@ -7,46 +7,50 @@ describe('Logging in', function() {
   applicationHelper()
 
   it('shows Facebook login in a popup', function() {
-    return this.app.client
-      // Click sign in button
-      .waitForVisible('button=Sign in')
-      .element('#content')
-      .click('button=Sign in')
-      // Wait for the modal to show up
-      .waitForVisible('.modal')
-      // Switch to the iframe inside the modal that contains the actual login buttons
-      .element('.modal iframe')
-      .then((iframe) => this.app.client.frame(iframe.value))
-      // Click the Facebook button once rendered
-      .waitForVisible('button=Continue with Facebook')
-      .click('button=Continue with Facebook')
-      // Wait for new window(s) to open
-      .waitUntil(() =>
-        this.app.client.getWindowCount().then((count) => count > 1)
-      )
-      .then(() => this.app.client.getWindowCount())
-      .then((windowCount) => assert.equal(windowCount, 2, 'Opened a popup'))
+    return (
+      this.app.client
+        // Click sign in button
+        .waitForVisible('button=Sign in')
+        .element('#content')
+        .click('button=Sign in')
+        // Wait for the modal to show up
+        .waitForVisible('.modal')
+        // Switch to the iframe inside the modal that contains the actual login buttons
+        .element('.modal iframe')
+        .then((iframe) => this.app.client.frame(iframe.value))
+        // Click the Facebook button once rendered
+        .waitForVisible('button=Continue with Facebook')
+        .click('button=Continue with Facebook')
+        // Wait for new window(s) to open
+        .waitUntil(() =>
+          this.app.client.getWindowCount().then((count) => count > 1)
+        )
+        .then(() => this.app.client.getWindowCount())
+        .then((windowCount) => assert.equal(windowCount, 2, 'Opened a popup'))
+    )
   })
 
   it('shows Google login in a popup', function() {
-    return this.app.client
-      // Click sign in button
-      .waitForVisible('button=Sign in')
-      .element('#content')
-      .click('button=Sign in')
-      // Wait for the modal to show up
-      .waitForVisible('.modal')
-      // Switch to the iframe inside the modal that contains the actual login buttons
-      .element('.modal iframe')
-      .then((iframe) => this.app.client.frame(iframe.value))
-      // Click the Google button once rendered
-      .waitForVisible('button=Continue with Google')
-      .click('button=Continue with Google')
-      // Wait for new window(s) to open
-      .waitUntil(() =>
-        this.app.client.getWindowCount().then((count) => count > 1)
-      )
-      .then(() => this.app.client.getWindowCount())
-      .then((windowCount) => assert.equal(windowCount, 2, 'Opened a popup'))
+    return (
+      this.app.client
+        // Click sign in button
+        .waitForVisible('button=Sign in')
+        .element('#content')
+        .click('button=Sign in')
+        // Wait for the modal to show up
+        .waitForVisible('.modal')
+        // Switch to the iframe inside the modal that contains the actual login buttons
+        .element('.modal iframe')
+        .then((iframe) => this.app.client.frame(iframe.value))
+        // Click the Google button once rendered
+        .waitForVisible('button=Continue with Google')
+        .click('button=Continue with Google')
+        // Wait for new window(s) to open
+        .waitUntil(() =>
+          this.app.client.getWindowCount().then((count) => count > 1)
+        )
+        .then(() => this.app.client.getWindowCount())
+        .then((windowCount) => assert.equal(windowCount, 2, 'Opened a popup'))
+    )
   })
 })
