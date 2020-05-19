730dbaa7b4ac2549d752fa47dcbb773e9f8703fd
Márton Salomváry <salomvary@gmail.com>
2020-05-19 13:26:09 +0200

    Notarize app for Catalina

---
+++ scripts/notarize.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { notarize } = require('electron-notarize')
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context
+  if (electronPlatformName !== 'darwin') {
+    return
+  }
+
+  const appName = context.packager.appInfo.productFilename
+
+  console.log(`Notarizing with Apple ID '${process.env.APPLEID}'`)
+  return notarize({
+    appBundleId: 'com.electron.soundcleod',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS
+  })
+}
